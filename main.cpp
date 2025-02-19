24a740cb1a1f1662c717cd9ec0772baf254db2f9
RyanPAalvim <ryalvim84@gmail.com>
2025-02-19 12:25:14 -0300

    read file added

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
+#include <random>
+#include <cmath>
+#include <fstream>
+
 using namespace std;
 
 #define N 6
 #define M 2
 
-void k_means(double x[][M], int k){
+struct point
+{
+    float vector[4];
+};
+
+
+void read_file()
+{
+    char line[200];
+    ifstream data_file("C:/Users/ryalv/Kmeans cpp/irisdata.txt");
+
+    if(!data_file.is_open())
+    {
+        cout << "Error";
+        return;
+    }
     
+    while(data_file.getline(line, 200))
+    {
+        cout << line << endl;
+    }
+    
+    data_file.close();
+}
+
+void k_means(double x[][M], int k)
+{
+    for(int i = 0; i < k; i++)
+    {
+
+    }    
 }
 
 int main()
 {
-    double x[N][M] = { {1, 2}, {2, 3}, {3, 4}, {6, 5}, {7, 6}, {8, 7}};
-    int k = 2;
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < M; j++)
-            cout << x[i][j] << ' ';
-        cout << endl;
-    }
-            
-    k_means(x, k);
+    int k, max_iter = 100;
+
+    cout << "How many groups would you like ?: ";
+    cin >> k;
+    cout << endl << "How many iterations ?: ";
+    cin >> max_iter;
+    cout << endl;
     
+    read_file();
+
     return 0;
+
 }
